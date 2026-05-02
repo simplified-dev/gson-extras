93e67a7de9f89bf8f4c2ff0e944e14a984f314dd
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-05-02 01:14:33 -0400

    Pin internal deps strictly (collections=a5f41e0, utils=5c6c96a, reflection=ed2e17c)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,9 @@ repositories {
 
 dependencies {
     // Simplified Libraries
-    api("com.github.simplified-dev:collections:master-SNAPSHOT")
-    api("com.github.simplified-dev:utils:master-SNAPSHOT")
-    api("com.github.simplified-dev:reflection:master-SNAPSHOT")
+    api("com.github.simplified-dev:collections") { version { strictly("a5f41e0") } }
+    api("com.github.simplified-dev:utils") { version { strictly("5c6c96a") } }
+    api("com.github.simplified-dev:reflection") { version { strictly("ed2e17c") } }
 
     // JetBrains Annotations
     api(libs.annotations)
